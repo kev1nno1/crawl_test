a057bc7672fa7adefb531ee6b2a7606a72a8a91b
kev1nno1 <cocacolabanana1510@gmail.com>
2021-08-09 22:05:24 +0700

    function to return date and content of an article from given url

--- main.py
+++ main.py
@@ -1,37 +1,53 @@
 import requests
 from bs4 import BeautifulSoup
-
+import datetime
+from newspaper import Article
+import json
 number= ["1","2","3","4","5"]
-for i in range(5):
-    url = "https://www.bbc.co.uk/search?q=crypto+currency&page="
+url = 'https://edition.cnn.com/2021/08/09/politics/donald-trump-justice-department-elections-republicans/index.html'
+# #url = 'http://fox13now.com/2013/12/30/new-year-new-laws-obamacare-pot-guns-and-drones/'
+# article = Article(url)
+# article.download()
+# article.parse()
+# article2 = article.text
+# print(article2)
 
-    new_link = url + number[i]
-    print(new_link)
-    response = requests.get(new_link)
-    soup = BeautifulSoup(response.content, "html.parser")
-    articles = soup.find_all('div', class_='ssrcss-1cbga70-Stack e1y4nx260')
+def date_and_content(url):
+    data = []
+    article = Article(url)
+    article.download()
+    article.parse()
+    content = article.text
+    date = article.publish_date
+    print(date)
+    data.append({
+        "publish date":date.isoformat(),
+        "content": content,
+    })
+    data2 = json.dumps(data)
+    return data2
 
+dc = date_and_content(url)
+print(dc)
 
-    for article in articles:
-        title = article.find('span',{"aria-hidden":"false"})
-
-        link = article.find('a').attrs["href"]
-        print('\n')
-        print(title.get_text())
-        print (link)
-        print ("--------------------")
-
-# response = requests.get("https://www.bbc.co.uk/search?q=crypto+page")
-# soup = BeautifulSoup(response.content, "html.parser")
-# articles = soup.find_all('div', class_='ssrcss-1cbga70-Stack e1y4nx260')
+# for i in range(5):
+#     url = "https://www.bbc.co.uk/search?q=crypto+currency&page="
+#
+#     new_link = url + number[i]
+#     print(new_link)
+#     response = requests.get(new_link)
+#     soup = BeautifulSoup(response.content, "html.parser")
+#     articles = soup.find_all('div', class_='ssrcss-1cbga70-Stack e1y4nx260')
 #
 #
-# for article in articles:
-#     title = article.find('span',{"aria-hidden":"false"})
+#     for article in articles:
+#         title = article.find('span',{"aria-hidden":"false"})
 #
-#     link = article.find('a').attrs["href"]
-#     print('\n')
-#     print(title.get_text())
-#     print (link)
-#     print ("--------------------")
+#         link = article.find('a').attrs["href"]
+#         print('\n')
+#         print(title.get_text())
+#         print (link)
+#         print ("--------------------")
+
+
 
