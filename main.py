f49a70a26f522317ed3eb97c237f220b73737764
kev1nno1 <cocacolabanana1510@gmail.com>
2021-08-10 01:51:22 +0700

    fix some bugs

--- main.py
+++ main.py
@@ -1,37 +1,100 @@
 import requests
 from bs4 import BeautifulSoup
 import datetime
+
 from newspaper import Article
 import json
-number= ["1","2","3","4","5"]
-url = 'https://edition.cnn.com/2021/08/09/politics/donald-trump-justice-department-elections-republicans/index.html'
-# #url = 'http://fox13now.com/2013/12/30/new-year-new-laws-obamacare-pot-guns-and-drones/'
-# article = Article(url)
-# article.download()
-# article.parse()
-# article2 = article.text
-# print(article2)
+from newspaper import build
 
+#cannot read vietnamese
+#date and time seeems weird
+#
+
+#This function returns all the url found in base url given ie
+# 'https://vnexpress.net', 'https://cnn.com...
+def get_articles_url(url):
+    url_set = []
+    setset = set()
+    articles = build(url, memoize_articles=False)
+
+    for article in articles.articles:
+        if article.url not in setset:
+            setset.add(article.url)
+            url_set.append(article.url)
+    return url_set
+
+
+#This function  return date, content, and article's title
+# and return in json format
 def date_and_content(url):
+
     data = []
-    article = Article(url)
+    article = Article(url )
     article.download()
     article.parse()
     content = article.text
     date = article.publish_date
     title = article.title
-    print(date)
-    data.append({
-        "title": title,
-        "date":date.isoformat(),
-        "content": content,
-    })
+
+    if date is not None:
+        date = date.strftime('%Y-%m-%d')
+        data.append({
+            "Title": title,
+            "date":date,
+            "Content": content,
+        })
+    else:
+        data.append({
+            "Title": title,
+            "date": "no date found",
+            "Content": content,
+        })
     data2 = json.dumps(data)
     return data2
 
-dc = date_and_content(url)
-print(dc)
+# Same as the function above but trying to make it read vietnamese news
+def date_and_content_vi(url):
+    data = []
+    article = Article(url, language='vi')
+    article.download()
+    article.parse()
+    content = article.text
+    date = article.publish_date
+    title = article.title
 
+    if date is not None:
+        date = date.strftime('%Y-%m-%d')
+        data.append({
+            "Title": title,
+            "date":date,
+            "Content": content,
+        })
+    else:
+        data.append({
+            "Title": title,
+            "date": "no date found",
+            "Content": content,
+        })
+    data2 = json.dumps(data)
+    return data2
+
+
+# TESTING SECTION
+
+
+#origin_url = 'https://vnexpress.net'
+origin_url = 'https://cnn.com'
+url_set = get_articles_url(origin_url)
+
+for url in url_set:
+    #print(url)
+    dt = date_and_content(url)
+    print(dt)
+
+
+# beautifulsoup example
+
+#number= ["1","2","3","4","5"]
 # for i in range(5):
 #     url = "https://www.bbc.co.uk/search?q=crypto+currency&page="
 #
